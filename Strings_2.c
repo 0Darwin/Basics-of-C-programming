dea8379e8e7774a0e45335a2a706f5ab0afe4b8f
Mohammed Benaissa <160794219+0Darwin@users.noreply.github.com>
2025-01-03 19:34:16 +0100

    Update

---
+++ Strings_2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+//exo 2
+void concatenate(char* str1, char* str2) {
+while (*str1) str1++; // Move to the end of the first string
+while (*str2) {
+*str1 = *str2;
+str1++;
+str2++;
+}
+*str1 = '\0'; // Null terminate the concatenated string
+}
+int main() {
+char str1[100], str2[50];
+printf("Enter first string: ");
+fgets(str1, sizeof(str1), stdin);
+printf("Enter second string: ");
+fgets(str2, sizeof(str2), stdin);
+concatenate(str1, str2);
+printf("Concatenated string: %s\n", str1);
+return 0;
+}
