986a2b58b7baf5a33234d95f5a1f76b48d01c202
Mohammed Benaissa <160794219+0Darwin@users.noreply.github.com>
2025-01-03 19:19:23 +0100

    Update

---
+++ STRINGS.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdio.h>
+int main(){
+printf("*TAKE A STRING AS INPUT AND DISPLAY ITS LENGTH: *");
+printf("\n");
+//exo 1
+char str[100];
+printf("Enter a string: ");
+fgets(str, sizeof(str), stdin);
+printf("Length of the string: %lu\n", strlen(str) - 1);
+return 0;
+}
