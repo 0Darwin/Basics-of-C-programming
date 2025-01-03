a1a7e0fe624be450b57c3c0ed902a33475271287
Mohammed Benaissa <160794219+0Darwin@users.noreply.github.com>
2025-01-03 16:43:36 +0100

    Update Basics_of_C_programming.c

--- Basics_of_C_programming.c
+++ Basics_of_C_programming.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 int main(){
+printf("* HELLO, WORLD! PROGRAM:*");
+
 //exo 1
+printf("\n");
 printf("Hello World\n");
 
+printf("\n");
+printf("* SUM OF TWO INTEGERS*");
+printf("\n");
+
 //exo 2
-int a,b;printf("Enter two integers: ");
+int a,b;printf("Enter two integers: ");
 scanf("%d %d", &a, &b);
 printf("Sum: %d\n", a + b);
+
+ printf("\n");
+ printf("AREA OF A RECTANGLE");
+ printf("\n");
 
 //exo 3
 float length, breadth;
@@ -14,6 +25,9 @@ printf("Enter length and breadth: ");
 scanf("%f %f", &length, &breadth);
 printf("Area: %.2f\n", length * breadth);
 
+printf("\n");
+printf("*SWAP TWO NUMBERS WITHOUT TEMPORARY VARIABLE:*");
+printf("\n");
 
 //exo 4
 int c, d;
@@ -23,6 +37,10 @@ c = c + d;
 d = c - d;
 c = c - d;
 printf("After swapping: c = %d, d = %d\n", c, d);
+
+printf("\n");
+printf("* CHECK IF A NUMBER IS EVEN OR ODD:*");
+printf("\n");
 
 //exo 5
 int num;
